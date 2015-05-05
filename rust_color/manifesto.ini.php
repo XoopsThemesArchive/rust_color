cfe9bf1e7f73581ae69267df718971efc8932e7a
mambax7 <mambax7@gmail.com>
2015-05-05 09:26:37 -0400

    Original files

---
+++ rust_color/manifesto.ini.php
@@ -0,0 +1,20 @@
+<?php
+/**
+
+[Manifesto]
+Name="rust_color"
+Depends=Legacy_RenderSystem
+Url="http://www.expiece.com/html/"
+Version="1.0"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=hi-debow @ EXTRAFFIC
+ScreenShot="screenshot.png"
+Description="3 columns fixed width 860px tableless CSS layout style theme"
+
+License="Attribution-Noncommercial 2.1 Japan"
+
+*/
+?>
